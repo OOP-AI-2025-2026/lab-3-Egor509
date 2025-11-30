30f2473892b9e1424463d12d84e926d57750b17a
Egor Sutaykin <9163280@stud.op.edu.ua>
2025-11-30 17:11:38 +0200

    Commit

--- src/main/java/org/example/task2/Order.java
+++ src/main/java/org/example/task2/Order.java
@@ -2,8 +2,8 @@ package org.example.task2;
 
 public class Order {
 
-    public long id;
-    public String customer;
+    private long id;
+    private String customer;
 
     public Order(long id, String customer) {
         this.id = id;
@@ -13,28 +13,30 @@ public class Order {
     public String formOrderBill(Cart cart) {
 
         StringBuilder builder = new StringBuilder();
-        builder.append("Order number ").append(id).append(" for customer ").append(customer);
+        builder.append("Order number ").append(this.id).append(" for customer ").append(this.customer);
         builder.append("\n------------------\n");
 
         double sum = 0.0;
 
-        for (int i = 0; i < cart.index; i++) {
+        for (int i = 0; i < cart.getCurrentSize(); i++) {
 
-            sum += cart.contents[i].price;
+            // Отримуємо item безпечно
+            Item currentItem = cart.getItemAtIndex(i);
+
+            sum += currentItem.getPrice(); // .getPrice() замість .price
 
             builder.append("Item id: ");
-            builder.append(cart.contents[i].id);
+            builder.append(currentItem.getId());
             builder.append(" name: ");
-            builder.append(cart.contents[i].name);
+            builder.append(currentItem.getName());
             builder.append(" price: ");
-            builder.append(cart.contents[i].price);
+            builder.append(currentItem.getPrice());
             builder.append("\n");
         }
 
         builder.append("------------------\n");
         builder.append("Total sum: ");
         builder.append(sum);
-
 
         return builder.toString();
     }
